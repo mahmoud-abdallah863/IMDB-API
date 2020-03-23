b15fe587b16293be72aaf952de21bdf68759ac67
mahmoud-abdallah863 <mabdallah863@gmail.com>
2020-03-23 09:15:42 +0200

    Add al files

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const scraper = require('./scrapper')
+
+const app = express()
+
+
+app.get('/', (req, res) => {
+    res.json({
+        message: "Everything is working fine"
+    })
+})
+
+
+app.get('/search/:title', (req, res) => {
+    scraper.searchMovies(req.params.title)
+        .then(movies => {
+            res.json(movies)
+        })
+})
+
+
+app.get('/movie/:imdbID', (req, res) => {
+    scraper.getMovie(req.params.imdbID)
+        .then(movie => {
+            res.json(movie)
+        })
+})
+
+
+
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
---
+++ scrapper.js
@@ -0,0 +1,141 @@
+const fetch = require('node-fetch')
+const cheerio = require('cheerio')
+
+const searchURl = 'https://www.imdb.com/find?&s=tt&ttype=ft&ref_=fn_ft&q='
+const movieURl = 'https://www.imdb.com/title/'
+
+function searchMovies(searchTerm){
+    if(searchCash[searchTerm]){
+        return Promise.resolve(searchCash[searchTerm])
+    }
+    return fetch(`${searchURl}${searchTerm}`)
+        .then(response => response.text())
+        .then(body => {
+            const $ = cheerio.load(body)
+    
+            const movies = []
+    
+            $('.findResult').each( (i, el) => {
+                const $element = $(el)
+                const $title = $element.find('td.result_text a')
+                const $image = $element.find('td a img')
+                const imdb_ID = $title.attr('href').match(/title\/(.*)\//)[1]
+    
+                const movie = {
+                    image: $image.attr('src'),
+                    title: $title.text(),
+                    imdb_ID
+                }
+                movies.push(movie)
+            })
+            searchCash[searchTerm] = movies
+    
+            return movies
+        })
+}
+
+
+const movieCach = {}
+const searchCash = {}
+
+
+function getMovie(imdbID){
+    if(movieCach[imdbID]){
+        return Promise.resolve(movieCach[imdbID])
+    }
+    return fetch(`${movieURl}${imdbID}`)
+        .then(response => response.text())
+        .then(body => {
+            const $ = cheerio.load(body)
+
+            const $title = $('.title_wrapper h1')
+            const title = $title.first().contents().filter( function() {
+                return this.type === 'text'
+            }).text().trim()
+
+            const videoLink = $('a[data-type="recommends"]').attr('href')
+
+            
+            const rating = $('.subtext').first().contents().filter( function() {
+                return this.type === 'text'
+            }).text().trim().substr(0, 5)
+
+            const time = $('time').first().contents().filter(function() {
+                return this.type === 'text';
+              }).text().trim()
+
+            const genres = []
+            let releasedIn = ""
+            let counter = 1
+            $('.subtext a').each( (i, el) => {
+                const genre = $(el).text()
+                if(counter == 4)
+                    releasedIn = genre.trim()
+                else{
+                    genres.push(genre)
+                    counter++
+                }
+            })
+
+
+            const imdbRating = $('span[itemprop=ratingValue]').text()
+            const poster = $('div.poster a img').attr('src')
+            const summary = $('.summary_text').text().trim()
+
+
+            const directors = []
+            const writers = []
+            const stars = []
+            $('.credit_summary_item a').each( (i, el) => {
+                const $href = $(el)
+                const $char = $href.attr('href').slice(-2)
+
+                const x = {
+                    name: $href.text(),
+                    link: $href.attr('href')
+                }      
+                if($char == 'dr')
+                    directors.push(x)
+                else{
+                    if($char == 'wr')
+                        writers.push(x)
+                    else if($char == 'sm')
+                        stars.push(x)
+                }
+            })
+            stars.pop()
+
+
+            const storyLine = $('.canwrap p span').text()
+
+            
+
+
+            const movie = {
+                imdbID, 
+                title,
+                videoLink: `https://www.imdb.com${videoLink}`,
+                rating,
+                time,
+                date_Published : releasedIn,
+                genres,
+                imdb_rating: imdbRating,
+                poster,
+                summary,
+                directors,
+                writers,
+                stars,
+                storyLine
+            }
+
+            movieCach[imdbID] = movie
+            return movie
+        })
+}
+
+
+
+module.exports = {
+    searchMovies,
+    getMovie
+}
