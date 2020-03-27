0b2726f82a77004fa7a42e30602de9e2cbb0ab2f
mahmoud-abdallah863 <mabdallah863@gmail.com>
2020-03-27 03:14:31 +0200

    Fixed port

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.get('/movie/:imdbID', (req, res) => {
 
 
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
 })
